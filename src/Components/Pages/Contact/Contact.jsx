aa03e5a5df192cf21383c145c29d1d7d07095786
Amir mohammad  Abdolahi <amirmuhaammaadd@gmail.com>
2024-08-03 10:10:00 +0200

    Add files via upload

---
+++ src/Components/Pages/Contact/Contact.jsx
@@ -0,0 +1,11 @@
+import { useReducer, useState } from "react";
+import { FiBarChart, FiBell, FiDollarSign, FiPlay } from "react-icons/fi";
+import { AnimatePresence, motion } from "framer-motion";
+
+
+const Contact = () => {
+
+  return <div></div>;
+};
+
+export default Contact;
